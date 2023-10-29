3b752309fb2a82b35535aac993c22d104704ec7f
manipriyan77 <manipriyangopalan@gmail.com>
2023-10-29 22:35:48 +0530

    learned the redux

---
+++ 21.Redux-thunk/src/App.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+import CreateCustomer from './features/customer/CreateCustomer';
+import Customer from './features/customer/Customer';
+import AccountOperations from './features/account/AccountOperations';
+import BalanceDisplay from './features/customer/BalanceDisplay';
+
+function App() {
+  const fullName = useSelector((state) => state.customer.fullName);
+
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      {fullName === '' ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ 21.Redux-thunk/src/features/customer/CreateCustomer.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { createCustomer } from './customerSlice';
+
+function Createcustomer() {
+  const [fullName, setFullName] = useState('');
+  const [nationalId, setNationalId] = useState('');
+
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    console.log('object');
+    dispatch(createCustomer(fullName, nationalId));
+  }
+
+  return (
+    <div>
+      <h2>Create new customer</h2>
+      <div className="inputs">
+        <div>
+          <label>Customer full name</label>
+          <input value={fullName} onChange={(e) => setFullName(e.target.value)} />
+        </div>
+        <div>
+          <label>National ID</label>
+          <input value={nationalId} onChange={(e) => setNationalId(e.target.value)} />
+        </div>
+        <button onClick={handleClick}>Create new customer</button>
+      </div>
+    </div>
+  );
+}
+
+export default Createcustomer;
